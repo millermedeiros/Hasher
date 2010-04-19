1608b8a78214b71e2064b52d284ef9b64bf355ab
Miller Medeiros <miller@millermedeiros.com>
2010-04-19 13:13:52 -0400

    improved documentation, updated EventDispatcher, some refactoring and started implementing support to IE <= 7.

--- src/HasherEvent.js
+++ src/HasherEvent.js
@@ -1,22 +1,22 @@
 /**
  * Hasher Event
  * @author Miller Medeiros <http://www.millermedeiros.com/>
- * @version 0.1 (2010/04/16)
+ * @version 0.2 (2010/04/18)
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
 
 /**
  * Constructor
  * - Creates a new HasherEvent Object.
- * - According to the HTML5 spec `hashchange` event should have `oldURL` and `newURL` properties. ( http://www.whatwg.org/specs/web-apps/current-work/multipage/history.html#event-hashchange )
+ * - According to the HTML5 spec `hashchange` event should have `oldURL` and `newURL` properties, since the only portion of the URL that changes is the hash I decided to use `oldHash` and `newHash` instead. ( http://www.whatwg.org/specs/web-apps/current-work/multipage/history.html#event-hashchange )
  * @param {String} eType	Hasher Event type.
- * @param {String} oldURL	Previous URL.
- * @param {String} newURL	Current URL.
+ * @param {String} oldHash	Previous Hash.
+ * @param {String} newHash	Current Hash.
  */
-var HasherEvent = function(eType, oldURL, newURL){
+var HasherEvent = function(eType, oldHash, newHash){
 	this.type = eType;
-	this.oldURL = oldURL;
-	this.newURL = newURL;
+	this.oldHash = oldHash;
+	this.newHash = newHash;
 }
 
 /**
@@ -24,7 +24,7 @@ var HasherEvent = function(eType, oldURL, newURL){
  * @return {String} A string representation of the object.
  */
 HasherEvent.prototype.toString = function(){
-	return '[HasherEvent type="'+ this.type +'" oldURL="'+ this.oldURL +'" newURL="'+ this.newURL +'"]';
+	return '[HasherEvent type="'+ this.type +'" oldHash="'+ this.oldHash +'" newHash="'+ this.newHash +'"]';
 };
 
 //-- Constants --//
