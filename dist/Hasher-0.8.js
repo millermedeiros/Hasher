26776fd7ec6942f3185335305b9368ee6cbb7eb5
Miller Medeiros <miller@millermedeiros.com>
2010-06-22 19:39:27 -0400

    moved things around, runned some lint..

--- dist/Hasher-0.8.js
+++ dist/Hasher-0.8.js
@@ -1,9 +1,9 @@
 /*!
  * Hasher
  * - History Manager for rich-media applications.
- * Includes: MM.EventDispatcher (0.7.2), MM.queryUtils (0.6)
+ * Includes: MM.EventDispatcher (0.7.2), MM.queryUtils (0.7)
  * @author Miller Medeiros <http://www.millermedeiros.com/>
- * @version 0.7 (2010/06/21)
+ * @version 0.8 (2010/06/22)
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
 /*
@@ -106,7 +106,7 @@ MM.EventDispatcher.prototype = {
  * MM.queryUtils
  * - utilities for query string manipulation
  * @author Miller Medeiros <http://www.millermedeiros.com/>
- * @version 0.6 (2010/06/21)
+ * @version 0.7 (2010/06/22)
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
 
@@ -129,7 +129,7 @@ MM.queryUtils = {
 	 */
 	getQueryString : function(url){
 		url = url || location.href; //used location.href to avoid bug on IE6 and pseudo query string inside location.hash
-		url = url.replace(/#.*/, ''); //removes hash (to avoid getting hash query)
+		url = url.replace(/#[\w\W]*/, ''); //removes hash (to avoid getting hash query)
 		var queryString = /\?[a-zA-Z0-9\=\&\%\$\-\_\.\+\!\*\'\(\)\,]+/.exec(url); //valid chars according to: http://www.ietf.org/rfc/rfc1738.txt
 		return (queryString)? decodeURIComponent(queryString[0]) : '';
 	},
@@ -177,7 +177,7 @@ MM.queryUtils = {
 	 * @return {Boolean} If parameter exist.
 	 */
 	hasParam : function(param, url){
-		var regexp = new RegExp('(\?|\&)'+ param +'\=', 'g'); //matches `?param=` or `&param=`
+		var regexp = new RegExp('(\\?|&)'+ param +'=', 'g'); //matches `?param=` or `&param=`
 		return regexp.test(this.getQueryString(url));
 	},
 	
@@ -190,38 +190,173 @@ MM.queryUtils = {
 		var query = [],
 			param;
 		for(param in obj){
-			query.push(param +'='+ obj[param]);
+			if(obj.hasOwnProperty(param)){ //avoid copying properties from the prototype
+				query.push(param +'='+ obj[param]);
+			}
 		}
 		return (query.length)? '?'+ query.join('&') : '';
 	}
 	
 };
 /*
+ * Hasher Event
+ * @author Miller Medeiros <http://www.millermedeiros.com/>
+ * @version 0.2 (2010/04/18)
+ * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+/**
+ * HasherEvent Object.
+ * <p>According to the HTML5 spec `hashchange` event should have `oldURL` and `newURL` properties, since the only portion of the URL that changes is the hash I decided to use `oldHash` and `newHash` instead. (http://www.whatwg.org/specs/web-apps/current-work/multipage/history.html#event-hashchange)</p>
+ * @param {String} eType	Hasher Event type.
+ * @param {String} oldHash	Previous Hash.
+ * @param {String} newHash	Current Hash.
+ * @constructor
+ */
+var HasherEvent = function(eType, oldHash, newHash){
+	/**
+	 * Event Type
+	 * @type String
+	 */
+	this.type = eType;
+	/**
+	 * Previous Hash value
+	 * @type String
+	 */
+	this.oldHash = oldHash;
+	/**
+	 * Current Hash value
+	 * @type String
+	 */
+	this.newHash = newHash;
+};
+
+/**
+ * Returns string representation of the HasherEvent
+ * @return {String} A string representation of the object.
+ */
+HasherEvent.prototype.toString = function(){
+	return '[HasherEvent type="'+ this.type +'" oldHash="'+ this.oldHash +'" newHash="'+ this.newHash +'"]';
+};
+
+//-- Constants --//
+
+/**
+ * Defines the value of the type property of an change event object.
+ * @type String
+ * @constant
+ */
+HasherEvent.CHANGE = 'change';
+
+ /**
+ * Defines the value of the type property of an init event object.
+ * @type String
+ * @constant
+ */
+HasherEvent.INIT = 'init';
+
+/**
+ * Defines the value of the type property of an stop event object.
+ * @type String
+ * @constant
+ */
+HasherEvent.STOP = 'stop';
+/*
  * Hasher
  * - History Manager for rich-media applications.
  * @author Miller Medeiros <http://www.millermedeiros.com/>
- * @version 0.7 (2010/06/21)
+ * @version 0.8 (2010/06/22)
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
-(function(window, document, undefined){
+(function(window, document, undef){
 	
 	
 	//== Private Vars ==//
-	
-	var	_oldHash, //{String} used to check if hash changed
-		_checkInterval, //stores setInterval reference (used to check if hash changed)
-		_frame, //iframe used for IE <= 7 
-		_isLegacyIE = /msie (6|7)/.test(navigator.userAgent.toLowerCase()) && !+"\v1"; //feature detection based on Andrea Giammarchi's solution: http://webreflection.blogspot.com/2009/01/32-bytes-to-know-if-your-browser-is-ie.html	
-	
-	
-	//== Public API ==//
 	
 	/**
 	 * Hasher
 	 * @namespace
 	 * @extends MM.EventDispatcher
 	 */
-	this.Hasher = new MM.EventDispatcher();
+	var	Hasher = new MM.EventDispatcher(),
+		_oldHash, //{String} used to check if hash changed
+		_checkInterval, //stores setInterval reference (used to check if hash changed)
+		_frame, //iframe used for IE <= 7
+		_isLegacyIE = /msie (6|7)/.test(navigator.userAgent.toLowerCase()) && !+"\v1"; //feature detection based on Andrea Giammarchi's solution: http://webreflection.blogspot.com/2009/01/32-bytes-to-know-if-your-browser-is-ie.html	
+	
+	
+	//== Private methods ==//
+	
+	/**
+	 * Dispatch `HasherEvent.CHANGE` and stores hash value.
+	 * @param {String} newHash	New Hash Value.
+	 * @private
+	 */
+	function _dispatchChange(newHash){
+		Hasher.dispatchEvent(new HasherEvent(HasherEvent.CHANGE, _oldHash, newHash));
+		_oldHash = newHash;
+	}
+	
+	/**
+	 * Function that checks if hash has changed. [HACK]
+	 * - used since most browsers don't dispatch the `onhashchange` event.
+	 * @private
+	 */
+	function _checkHash(){
+		var curHash = Hasher.getHash();
+		if(curHash != _oldHash){
+			_dispatchChange(curHash);
+		}
+	}
+	
+	/**
+	 * Check if browser history state has changed on IE <= 7. [HACK]
+	 * - used since IE 6,7 doesn't generates new history record on hashchange.
+	 * @private
+	 */
+	function _checkHistoryLegacyIE(){
+		var windowHash = Hasher.getHash(),
+			frameHash = _frame.contentWindow.frameHash;
+		if(frameHash != windowHash && frameHash != _oldHash){ //detect changes made pressing browser history buttons. Workaround since history.back() and history.forward() doesn't update hash value on IE6/7 but updates content of the iframe.
+			Hasher.setHash(frameHash);
+			_dispatchChange(frameHash);
+		}else if(windowHash != _oldHash){ //detect if hash changed (manually or using setHash)
+			if(frameHash != windowHash){
+				_updateFrame();
+			}
+			_dispatchChange(windowHash);
+		}
+	}
+	
+	/**
+	 * Creates iframe used to record history state on IE <= 7. [HACK]
+	 * @private
+	 */
+	function _createFrame(){
+		_frame = document.createElement('iframe');
+		_frame.src = 'javascript:false';
+		_frame.style.display = 'none';
+		document.body.appendChild(_frame);
+	}
+	
+	/**
+	 * Update iframe content, generating a history record and saving current hash/title on IE <= 7. [HACK]
+	 * - based on Really Simple History, SWFAddress and YUI.history solutions.
+	 * @private
+	 */
+	function _updateFrame(){
+		var frameDoc = _frame.contentWindow.document;
+		frameDoc.open();
+		frameDoc.write('<html><head><title>'+ Hasher.getTitle() +'</title><script type="text/javascript">var frameHash="'+ Hasher.getHash() +'";</script></head><body>&nbsp;</body></html>'); //stores current hash inside iframe.
+		frameDoc.close();
+	}
+	
+	
+	//== Public API ==//
+	
+	
+	//register Hasher to the global scope
+	window.Hasher = Hasher;
 	
 	/**
 	 * Start listening/dispatching changes in the hash/history.
@@ -363,133 +498,4 @@ MM.queryUtils = {
 		return MM.queryUtils.getParamValue(paramName, this.getHash() );
 	};
 	
-	//== Private methods ==//
-	
-	/**
-	 * Dispatch `HasherEvent.CHANGE` and stores hash value.
-	 * @param {String} newHash	New Hash Value.
-	 * @private
-	 */
-	function _dispatchChange(newHash){
-		Hasher.dispatchEvent(new HasherEvent(HasherEvent.CHANGE, _oldHash, newHash));
-		_oldHash = newHash;
-	}
-	
-	/**
-	 * Function that checks if hash has changed. [HACK]
-	 * - used since most browsers don't dispatch the `onhashchange` event.
-	 * @private
-	 */
-	function _checkHash(){
-		var curHash = Hasher.getHash();
-		if(curHash != _oldHash){
-			_dispatchChange(curHash);
-		}
-	}
-	
-	/**
-	 * Check if browser history state has changed on IE <= 7. [HACK]
-	 * - used since IE 6,7 doesn't generates new history record on hashchange.
-	 * @private
-	 */
-	function _checkHistoryLegacyIE(){
-		var windowHash = Hasher.getHash(),
-			frameHash = _frame.contentWindow.frameHash;
-		if(frameHash != windowHash && frameHash != _oldHash){ //detect changes made pressing browser history buttons. Workaround since history.back() and history.forward() doesn't update hash value on IE6/7 but updates content of the iframe.
-			Hasher.setHash(frameHash);
-			_dispatchChange(frameHash);
-		}else if(windowHash != _oldHash){ //detect if hash changed (manually or using setHash)
-			if(frameHash != windowHash){
-				_updateFrame();
-			}
-			_dispatchChange(windowHash);
-		}
-	}
-	
-	/**
-	 * Creates iframe used to record history state on IE <= 7. [HACK]
-	 * @private
-	 */
-	function _createFrame(){
-		_frame = document.createElement('iframe');
-		_frame.src = 'javascript:false';
-		_frame.style.display = 'none';
-		document.body.appendChild(_frame);
-	}
-	
-	/**
-	 * Update iframe content, generating a history record and saving current hash/title on IE <= 7. [HACK]
-	 * - based on Really Simple History, SWFAddress and YUI.history solutions.
-	 * @private
-	 */
-	function _updateFrame(){
-		var frameDoc = _frame.contentWindow.document;
-		frameDoc.open();
-		frameDoc.write('<html><head><title>'+ Hasher.getTitle() +'</title><script type="text/javascript">var frameHash="'+ Hasher.getHash() +'";</script></head><body>&nbsp;</body></html>'); //stores current hash inside iframe.
-		frameDoc.close();
-	}
-	
 })(window, document);
-/*
- * Hasher Event
- * @author Miller Medeiros <http://www.millermedeiros.com/>
- * @version 0.2 (2010/04/18)
- * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
- */
-
-/**
- * HasherEvent Object.
- * <p>According to the HTML5 spec `hashchange` event should have `oldURL` and `newURL` properties, since the only portion of the URL that changes is the hash I decided to use `oldHash` and `newHash` instead. (http://www.whatwg.org/specs/web-apps/current-work/multipage/history.html#event-hashchange)</p>
- * @param {String} eType	Hasher Event type.
- * @param {String} oldHash	Previous Hash.
- * @param {String} newHash	Current Hash.
- * @constructor
- */
-var HasherEvent = function(eType, oldHash, newHash){
-	/**
-	 * Event Type
-	 * @type String
-	 */
-	this.type = eType;
-	/**
-	 * Previous Hash value
-	 * @type String
-	 */
-	this.oldHash = oldHash;
-	/**
-	 * Current Hash value
-	 * @type String
-	 */
-	this.newHash = newHash;
-};
-
-/**
- * Returns string representation of the HasherEvent
- * @return {String} A string representation of the object.
- */
-HasherEvent.prototype.toString = function(){
-	return '[HasherEvent type="'+ this.type +'" oldHash="'+ this.oldHash +'" newHash="'+ this.newHash +'"]';
-};
-
-//-- Constants --//
-
-/**
- * Defines the value of the type property of an change event object.
- * @type String
- * @constant
- */
-HasherEvent.CHANGE = 'change';
-
- /**
- * Defines the value of the type property of an init event object.
- * @type String
- * @constant
- */
-HasherEvent.INIT = 'init';
-
-/**
- * Defines the value of the type property of an stop event object.
- * @type String
- * @constant
- */
-HasherEvent.STOP = 'stop';
--- src/Hasher.js
+++ src/Hasher.js
@@ -2,28 +2,98 @@
  * Hasher
  * - History Manager for rich-media applications.
  * @author Miller Medeiros <http://www.millermedeiros.com/>
- * @version 0.7 (2010/06/21)
+ * @version 0.8 (2010/06/22)
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
-(function(window, document, undefined){
+(function(window, document, undef){
 	
 	
 	//== Private Vars ==//
-	
-	var	_oldHash, //{String} used to check if hash changed
-		_checkInterval, //stores setInterval reference (used to check if hash changed)
-		_frame, //iframe used for IE <= 7 
-		_isLegacyIE = /msie (6|7)/.test(navigator.userAgent.toLowerCase()) && !+"\v1"; //feature detection based on Andrea Giammarchi's solution: http://webreflection.blogspot.com/2009/01/32-bytes-to-know-if-your-browser-is-ie.html	
-	
-	
-	//== Public API ==//
 	
 	/**
 	 * Hasher
 	 * @namespace
 	 * @extends MM.EventDispatcher
 	 */
-	this.Hasher = new MM.EventDispatcher();
+	var	Hasher = new MM.EventDispatcher(),
+		_oldHash, //{String} used to check if hash changed
+		_checkInterval, //stores setInterval reference (used to check if hash changed)
+		_frame, //iframe used for IE <= 7
+		_isLegacyIE = /msie (6|7)/.test(navigator.userAgent.toLowerCase()) && !+"\v1"; //feature detection based on Andrea Giammarchi's solution: http://webreflection.blogspot.com/2009/01/32-bytes-to-know-if-your-browser-is-ie.html	
+	
+	
+	//== Private methods ==//
+	
+	/**
+	 * Dispatch `HasherEvent.CHANGE` and stores hash value.
+	 * @param {String} newHash	New Hash Value.
+	 * @private
+	 */
+	function _dispatchChange(newHash){
+		Hasher.dispatchEvent(new HasherEvent(HasherEvent.CHANGE, _oldHash, newHash));
+		_oldHash = newHash;
+	}
+	
+	/**
+	 * Function that checks if hash has changed. [HACK]
+	 * - used since most browsers don't dispatch the `onhashchange` event.
+	 * @private
+	 */
+	function _checkHash(){
+		var curHash = Hasher.getHash();
+		if(curHash != _oldHash){
+			_dispatchChange(curHash);
+		}
+	}
+	
+	/**
+	 * Check if browser history state has changed on IE <= 7. [HACK]
+	 * - used since IE 6,7 doesn't generates new history record on hashchange.
+	 * @private
+	 */
+	function _checkHistoryLegacyIE(){
+		var windowHash = Hasher.getHash(),
+			frameHash = _frame.contentWindow.frameHash;
+		if(frameHash != windowHash && frameHash != _oldHash){ //detect changes made pressing browser history buttons. Workaround since history.back() and history.forward() doesn't update hash value on IE6/7 but updates content of the iframe.
+			Hasher.setHash(frameHash);
+			_dispatchChange(frameHash);
+		}else if(windowHash != _oldHash){ //detect if hash changed (manually or using setHash)
+			if(frameHash != windowHash){
+				_updateFrame();
+			}
+			_dispatchChange(windowHash);
+		}
+	}
+	
+	/**
+	 * Creates iframe used to record history state on IE <= 7. [HACK]
+	 * @private
+	 */
+	function _createFrame(){
+		_frame = document.createElement('iframe');
+		_frame.src = 'javascript:false';
+		_frame.style.display = 'none';
+		document.body.appendChild(_frame);
+	}
+	
+	/**
+	 * Update iframe content, generating a history record and saving current hash/title on IE <= 7. [HACK]
+	 * - based on Really Simple History, SWFAddress and YUI.history solutions.
+	 * @private
+	 */
+	function _updateFrame(){
+		var frameDoc = _frame.contentWindow.document;
+		frameDoc.open();
+		frameDoc.write('<html><head><title>'+ Hasher.getTitle() +'</title><script type="text/javascript">var frameHash="'+ Hasher.getHash() +'";</script></head><body>&nbsp;</body></html>'); //stores current hash inside iframe.
+		frameDoc.close();
+	}
+	
+	
+	//== Public API ==//
+	
+	
+	//register Hasher to the global scope
+	window.Hasher = Hasher;
 	
 	/**
 	 * Start listening/dispatching changes in the hash/history.
@@ -164,71 +234,5 @@
 	Hasher.getHashQueryParam = function(paramName){
 		return MM.queryUtils.getParamValue(paramName, this.getHash() );
 	};
-	
-	//== Private methods ==//
-	
-	/**
-	 * Dispatch `HasherEvent.CHANGE` and stores hash value.
-	 * @param {String} newHash	New Hash Value.
-	 * @private
-	 */
-	function _dispatchChange(newHash){
-		Hasher.dispatchEvent(new HasherEvent(HasherEvent.CHANGE, _oldHash, newHash));
-		_oldHash = newHash;
-	}
-	
-	/**
-	 * Function that checks if hash has changed. [HACK]
-	 * - used since most browsers don't dispatch the `onhashchange` event.
-	 * @private
-	 */
-	function _checkHash(){
-		var curHash = Hasher.getHash();
-		if(curHash != _oldHash){
-			_dispatchChange(curHash);
-		}
-	}
-	
-	/**
-	 * Check if browser history state has changed on IE <= 7. [HACK]
-	 * - used since IE 6,7 doesn't generates new history record on hashchange.
-	 * @private
-	 */
-	function _checkHistoryLegacyIE(){
-		var windowHash = Hasher.getHash(),
-			frameHash = _frame.contentWindow.frameHash;
-		if(frameHash != windowHash && frameHash != _oldHash){ //detect changes made pressing browser history buttons. Workaround since history.back() and history.forward() doesn't update hash value on IE6/7 but updates content of the iframe.
-			Hasher.setHash(frameHash);
-			_dispatchChange(frameHash);
-		}else if(windowHash != _oldHash){ //detect if hash changed (manually or using setHash)
-			if(frameHash != windowHash){
-				_updateFrame();
-			}
-			_dispatchChange(windowHash);
-		}
-	}
-	
-	/**
-	 * Creates iframe used to record history state on IE <= 7. [HACK]
-	 * @private
-	 */
-	function _createFrame(){
-		_frame = document.createElement('iframe');
-		_frame.src = 'javascript:false';
-		_frame.style.display = 'none';
-		document.body.appendChild(_frame);
-	}
-	
-	/**
-	 * Update iframe content, generating a history record and saving current hash/title on IE <= 7. [HACK]
-	 * - based on Really Simple History, SWFAddress and YUI.history solutions.
-	 * @private
-	 */
-	function _updateFrame(){
-		var frameDoc = _frame.contentWindow.document;
-		frameDoc.open();
-		frameDoc.write('<html><head><title>'+ Hasher.getTitle() +'</title><script type="text/javascript">var frameHash="'+ Hasher.getHash() +'";</script></head><body>&nbsp;</body></html>'); //stores current hash inside iframe.
-		frameDoc.close();
-	}
 	
 })(window, document);
